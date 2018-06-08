290740fceaaf515d4b83d6d880564c7d2802bac6
Art <partypeopleland@gmail.com>
2018-06-08 11:18:17 +0800

    first green

---
+++ src/Calculator.js
@@ -0,0 +1,31 @@
+function Calculator() { }
+
+Calculator.prototype.Compute = function (a, b) {
+    return this.Cal(a, b)
+}
+
+Calculator.factory = function (type) {
+    let constr = type
+
+    if (typeof Calculator[constr] !== "function") {
+        throw {
+            name: "Error",
+            message: constr + " dosen't exist"
+        }
+    }
+
+    if (typeof Calculator[constr].prototype.drive !== "function") {
+        Calculator[constr].prototype = new Calculator()
+    }
+
+    return new Calculator[constr]()
+}
+
+Calculator.add = function () {
+    this.Cal = function (a, b) {
+        return a + b
+    }
+}
+
+
+module.exports = Calculator
