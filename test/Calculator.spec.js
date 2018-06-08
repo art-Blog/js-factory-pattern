6bf77b8c9a5323bd8b2016fc5303f95fcc3191d2
Art <partypeopleland@gmail.com>
2018-06-08 11:32:07 +0800

    last red

--- test/Calculator.spec.js
+++ test/Calculator.spec.js
@@ -9,6 +9,7 @@ describe('Calculator', () => {
         { args: { a: 3, b: 5 }, operator: 'add', expected: 8 },
         { args: { a: 3, b: 5 }, operator: 'sub', expected: -2 },
         { args: { a: 3, b: 5 }, operator: 'mul', expected: 15 },
+        { args: { a: 3, b: 5 }, operator: 'div', expected: 0.6 },
     ]
     dataSet.forEach(data => {
         it(`${data.operator} ${data.args.a} and ${data.args.b} should equal ${data.expected}`, () => {
